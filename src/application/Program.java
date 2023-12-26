101cb27d16a9f2ff86f7ad4a3e49a154a3cbd2c4
FabiusDev <fsantos93x@gmail.com>
2023-12-26 20:20:52 -0300

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Chess_System {
+}
