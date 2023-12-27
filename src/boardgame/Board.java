50cd4a66587b4b1eca1d47029036f91aa463776d
FabiusDev <fsantos93x@gmail.com>
2023-12-27 12:48:24 -0300

    "pieces Rook and King added"

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,6 +36,12 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 		
 	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+		
+	}
 
 	
 	
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -9,6 +12,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetUp();
 		
 	}
 	
@@ -24,6 +28,14 @@ public class ChessMatch {
 		}
 		
 		return mat;
+	}
+	
+	private void initialSetUp() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 5));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
+
 	}
 	
 
