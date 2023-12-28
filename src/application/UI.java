1d2b1c20a75d19d13fcbb7e64bdaed3eb94998c1
FabiusDev <fsantos93x@gmail.com>
2023-12-28 17:24:22 -0300

    set classes

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,7 +54,19 @@ public class UI {
 		for(int i=0; i<pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j=0; j<pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("  a b c d e f g h");
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for(int i=0; i<pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j=0; j<pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
@@ -63,9 +75,13 @@ public class UI {
 	}
 	
 	
-	private static void printPiece(ChessPiece piece) {
+	
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print(ANSI_PURPLE_BACKGROUND);
+		}
     	if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
