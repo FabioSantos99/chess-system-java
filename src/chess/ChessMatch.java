1adaf422f901d3ea08a16ca3cc58d586b0205014
FabiusDev <fsantos93x@gmail.com>
2023-12-28 18:47:35 -0300

    Setting possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -84,6 +84,13 @@ public class ChessMatch {
 		}
 	}
 	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+		
+	}
+	
 	private void nextTurn() {
 		turn++;
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
