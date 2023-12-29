b3983f17cccaad51b556f39bf63b06f7a1f2fa3a
FabiusDev <fsantos93x@gmail.com>
2023-12-29 15:28:46 -0300

    piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,6 +8,8 @@ public abstract class ChessPiece extends Piece {
 
 	
 	private Color color;
+	private int moveCount;
+	
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,6 +19,19 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
 	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
