f2dd446bb6bb628a76007b5d5d3b24845f3e61d4
FabiusDev <fsantos93x@gmail.com>
2023-12-30 22:39:00 -0300

    Knight

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -12,8 +13,8 @@ import chess.ChessPosition;
 
 public class Program {
 
-	public static void main(String[] args) {
-
+	public static void main (String[] args){
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
@@ -55,6 +56,7 @@ public class Program {
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
 		
+	
 	}
 		
 
